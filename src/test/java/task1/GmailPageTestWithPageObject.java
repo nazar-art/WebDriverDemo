eb6d85667f8c40d34e128dd0eb1de681a6af4907
Nazar Lelyak <nazar.lelyak@gmail.com>
2014-06-11 13:05:53 +0300

    task 1 works correctly

--- src/test/java/task1/GmailPageTestWithPageObject.java
+++ src/test/java/task1/GmailPageTestWithPageObject.java
@@ -1,11 +1,8 @@
 package task1;
 
 import org.apache.log4j.Logger;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -29,62 +26,32 @@ public class GmailPageTestWithPageObject {
 
     @BeforeTest
     public void setUp() {
-        try {
-            driver.get(GmailLoginPage.LOGIN_URL);
-            driver.manage().window().maximize();
-            GmailLoginPage loginPage = new GmailLoginPage();
-            page = loginPage.loginAs(USER_LOGIN, USER_PASSWORD);
-        } catch (Exception e) {
-            log.error("GmailPageClassTest - setUp() fail", e);
-            Assert.fail("GmailPageClassTest - setUp() fail", e.getCause());
-        }
+        driver.get(GmailLoginPage.LOGIN_URL);
+//        driver.manage().window().maximize();
+        GmailLoginPage loginPage = new GmailLoginPage();
+        page = loginPage.loginAs(USER_LOGIN, USER_PASSWORD);
     }
 
     @Test(groups = "GMAIL_PAGE")
     public void testIfDraftFolderContainsSavedAndClosedDraft() {
-        try {
-            (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-                @Override
-                public Boolean apply(WebDriver d) {
-                    return d.findElement(By.xpath("//*[@id=':4e']/div/div")).isEnabled();
-                }
-            });
-            page.pressComposeButton();
-            page.sendTextToMessageFrame(TestUtils.TEST_MESSAGE_FOR_GMAIL_PAGE_TEST);
-            page.clickDraftLink();
-            List<WebElement> allMessages = page.takeAllMessages();
-            Assert.assertTrue(letterContainsTextMessage(allMessages, TestUtils.TEST_MESSAGE_FOR_GMAIL_PAGE_TEST),
-                    "any letter doesn't contain test message");
-        } catch (Exception e) {
-            log.error("GmailPageClassTest - testIfDraftFolderContainsSavedAndClosedDraft() fail", e);
-            Assert.fail("GmailPageClassTest - testIfDraftFolderContainsSavedAndClosedDraft() fail", e);
-        }
+        page.pressComposeButton();
+        page.sendTextToMessageFrame(TestUtils.TEST_MESSAGE_FOR_GMAIL_PAGE_TEST);
+        page.clickDraftLink();
+        List<WebElement> allMessages = page.takeAllMessages();
+        Assert.assertTrue(letterContainsTextMessage(allMessages, TestUtils.TEST_MESSAGE_FOR_GMAIL_PAGE_TEST),
+                "any letter doesn't contain test message");
     }
 
     @AfterTest
     public void tearDown() {
-        try {
-            page.clickProfileOptionMenu();
-            page.clickSignOut();
-
-            (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-                @Override
-                public Boolean apply(WebDriver d) {
-                    return d.getTitle().toLowerCase().startsWith("gmail");
-                }
-            });
-        } catch (Exception e) {
-            log.error("GmailPageClassTest - tearDown() fail", e);
-            Assert.fail("GmailPageClassTest - tearDown() fail", e.getCause());
-        } finally {
-            DriverManager.closeQuietly();
-        }
+        DriverManager.closeQuietly();
     }
 
     /**
      * Check if list of letters from page contains text message.
+     *
      * @param webElementList list of web elements.
-     * @param message test message.
+     * @param message        test message.
      * @return if letter contain message true, otherwise false.
      */
     public boolean letterContainsTextMessage(List<WebElement> webElementList, String message) {
